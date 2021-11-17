085a8422c8ad0f1a5fcdd030f9213ff9b115fb9e
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-11-17 14:55:40 -0500

    commit tree c4b9e0258f15675b549013e9977ff561e91b9a0a

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,25 @@
+module Memorable
+  module ClassMethods
+    def find_by_name(name)
+      self.all.detect { |a| a.name == name }
+    end
+
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+
+  module InstanceMethods
+    def to_param
+      name.downcase.gsub(" ", "-")
+    end
+
+    def initialize
+      self.class.all << self
+    end
+  end
+end
